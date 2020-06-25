e65d90af8a6238ff76595e59562778c1cfe54c00
MinKyeong Choi <59993071+alsrud9909@users.noreply.github.com>
2020-06-25 10:13:33 +0900

    이미지 인 경우에만 인식 가능. 윗 부분 웹캠인 경우 정확도를 높히도록 수정필요

--- 200623/명함인식.py
+++ 200623/명함인식.py
@@ -28,7 +28,7 @@ def auto_scan_image_via_webcam():
         gray = cv2.GaussianBlur(gray, (3, 3), 0)
         edged = cv2.Canny(gray, 75, 200)
 
-        print("step1 : edge detection")
+        print("edge detection")
 
         (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
@@ -44,23 +44,22 @@ def auto_scan_image_via_webcam():
                 camSize = frame.shape[0] * frame.shape[1]
                 ratio = contourSize / camSize
 
-                '''print(contourSize)
+                print(contourSize)
                 print(camSize)
-                print(ratio)'''
+                print(ratio)
 
                 ret, frame = cap.read()
 
                 if ratio > 0.1:
                     screenCnt = approx
+                    
                     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                     img = Image.fromarray(gray)  # frame으로 넣어도 된다.
+                    
                     txt = pytesseract.image_to_string(img, lang='kor+eng')
                     print(txt)
 
-                    flag = 1;
-                    break
-
-        if flag == 1 :break
+                    return 
 
         if len(screenCnt) == 0:
             cv2.imshow('WebCam', frame)
@@ -68,8 +67,6 @@ def auto_scan_image_via_webcam():
             continue
 
         else :
-            print("step2 : find contours of paper")
-
             cv2.drawContours(frame, [screenCnt], -1, (0, 255, 0), 2)
             cv2.imshow('WebCam', frame)
 
